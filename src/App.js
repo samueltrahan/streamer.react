ad15170f8ada43a2b899f08484f061202a7f9a2b
Samuel Trahan <samtrahan91@gmail.com>
2020-10-05 16:00:06 -0500

    semantic ui add

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-import React from 'react';
-import NavBar from './components/NavBar'
+import React, { useState } from "react";
+import NavBar from "./components/NavBar";
 
 const App = () => {
+  const [videos, setVideos] = useState([]);
+  const [tweets, setTweets] = useState([]);
+
   return (
     <>
-    <NavBar />
+      <NavBar />
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
