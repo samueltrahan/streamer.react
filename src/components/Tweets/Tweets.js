777a2d62c8b0dd5cadc0d8c8c3cbb3292cbe4009
Samuel Trahan <samtrahan91@gmail.com>
2020-10-12 10:39:36 -0500

    more details about steamers in constant page

--- src/components/Tweets/Tweets.js
+++ src/components/Tweets/Tweets.js
@@ -11,7 +11,7 @@ export default function Tweets() {
     <Timeline 
     dataSource={{
       sourceType: 'profile',
-      screenName: 'NICKMERCS'
+      screenName: 'scump'
     }}
     options={{
       width: '40vw',
--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,58 @@
-const STREAMERS = ['Nickmercs', 'Scump', 'Cloaksy', 'TimTheTatman', 'Aydan', 'Mutex', 'Speros', 'Huskers', 'CourageJD', 'Nadeshot', 'Dr. Disrespect']
+const STREAMERS = [{
+  Nickmercs: {
+    twitterName: 'NICKMERCS',
+    twitchName: 'NICKMERCS',
+    youtubeName: 'NICKMERCS'
+  }, 
+  Scump: {
+    twitterName: 'scump',
+    twitchName: 'scump',
+    youtubeName: 'scump'
+  }, 
+  Cloaksy: {
+    twitterName: 'cloaksy',
+    twitchName: 'cloaksy',
+    youtubeName: 'cloaksy',
+  }, 
+  TimTheTatman: {
+    twitterName: 'timthetatman',
+    twitchName: 'timthetatman',
+    youtubeName: 'timthetatman'
+  }, 
+  Aydan: {
+    twitterName: 'aydan',
+    twitchName: 'aydan',
+    youtubeName: 'aydan'
+  }, 
+  Mutex: {
+    twitterName: 'MuTeX',
+    twitchName:  'MuTeX',
+    youtubeName: 'MuTeX',
+  }, 
+  Speros: {
+    twitterName: 'Speros_OG',
+    twitchName: 'Speros_OG',
+    youtubeName: 'Speros'
+  }, 
+  Huskers: {
+    twitterName: 'NRG Huskers',
+    twitchName: 'HusKerrs',
+    youtubeName: 'HusKerrs'
+  }, 
+  CourageJD: {
+    twitterName: 'CouRageJD',
+    twitchName: 'CouRageJD',
+    youtubeName: 'CouRage'
+  }, 
+  Nadeshot: {
+    twitterName: '100T Nadeshot',
+    twitchName: 'Nadeshot',
+    youtubeName: 'NadeShot Plays'
+  }, 
+  'Dr. Disrespect': {
+    twitterName: 'drdisrespect',
+    youtubeName: 'DrDisRespect'
+  }}]
 
 export {
   STREAMERS
